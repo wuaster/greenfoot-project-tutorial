04b2c1fc3e618b9dc9e4bc873262bf677701dcde
nathaliekng <41026746+nathaliekng@users.noreply.github.com>
2018-07-09 20:57:25 -0400

    added moving for ship

--- Shootsz/Player.java
+++ Shootsz/Player.java
@@ -12,8 +12,20 @@ public class Player extends Spaceship
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 4;
     public void act() 
     {
-        // Add your action code here.
-    }    
+        if(Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + speed);
+        }
+        if(Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - speed);
+        }
+        if(Greenfoot.isKeyDown("a")) {
+            setLocation (getX() - speed, getY());
+        }
+        if(Greenfoot.isKeyDown("d")) {
+            setLocation (getX() + speed, getY());
+        }
+    }
 }
